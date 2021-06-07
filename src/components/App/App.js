8a134f415b2550598282e96700deab02ece9fa26
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-06-07 19:57:58 +0300

    finished 30%

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,19 +7,33 @@ class App extends Component {
   state = {
     data: [],
     currentPage: 1,
+    searchQuery: "",
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchImages();
+    }
+  }
+
   onChangeQuery = (query) => {
-    const { currentPage } = this.state;
+    this.setState({ searchQuery: query, currentPage: 1, data: [] });
+  };
+
+  fetchImages = () => {
+    const { currentPage, searchQuery } = this.state;
     const apiKey = "18864505-6c3c7593910f8166537b8d98b";
     axios
       .get(
-        `https://pixabay.com/api/?q=${query}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12
+        `https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12
         `
       )
       .then(({ data }) => {
         console.log(data.hits);
-        this.setState({ data: data.hits });
+        this.setState((prevState) => ({
+          data: [...prevState.data, ...data.hits],
+          currentPage: prevState.currentPage + 1,
+        }));
       })
       .catch((error) => console.log(error));
   };
@@ -27,15 +41,22 @@ class App extends Component {
   render() {
     const { data } = this.state;
     return (
-      <div>
+      <div className="App">
         <Searchbar onSubmit={this.onChangeQuery} />
         <ul className="ImageGallery">
           {data.map(({ id, webformatURL, tags }) => (
             <li className="ImageGalleryItem" key={id}>
-              <img src={webformatURL} alt={tags} />
+              <img
+                src={webformatURL}
+                alt={tags}
+                className="ImageGalleryItem-image"
+              />
             </li>
           ))}
         </ul>
+        <button type="button" onClick={this.fetchImages} className="Button">
+          Load more
+        </button>
       </div>
     );
   }
